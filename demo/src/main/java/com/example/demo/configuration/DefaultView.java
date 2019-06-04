902e9d2f8fe607426d4d730edeef6ee010e9dd6c
zhangguofeng <1372554219@qq.com>
2019-06-04 22:10:58 +0800

    后台拦截器

--- demo/src/main/java/com/example/demo/configuration/DefaultView.java
+++ demo/src/main/java/com/example/demo/configuration/DefaultView.java
@@ -1,7 +1,11 @@
 package com.example.demo.configuration;
 
+import com.example.demo.interceptor.LoginHandlerInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
@@ -18,11 +22,24 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 @Configuration
 public class DefaultView  extends WebMvcConfigurerAdapter {
 
-//
-//    @Override    public void addViewControllers(ViewControllerRegistry registry) {
-//        registry.addViewController("/hello").setViewName("index");
-//        registry.setOrder(Ordered.HIGHEST_PRECEDENCE);
-//        super.addViewControllers(registry);
-//    }
+    @Autowired
+
+    private LoginHandlerInterceptor loginInterceptor;
+
+
+    // 这个方法是用来配置静态资源的，比如html，js，css，等等
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+
+    }
+
+    // 这个方法用来注册拦截器，我们自己写好的拦截器需要通过这里添加注册才能生效
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        //这条语句的作用是对所有路径应用拦截器，除了 /index.html 以及css、js、images目录下的文件,因为static是静态资源直接访问static目录的文件 。
+        registry.addInterceptor(loginInterceptor).addPathPatterns("/**").excludePathPatterns("/index.html","/css/**","/js/**","/images/**");
+        super.addInterceptors(registry);    //较新
+
+    }
 }
 
