51a037a1afde008820565c75089e01cafc8e7b79
zhangguofeng <1372554219@qq.com>
2019-06-30 19:07:15 +0800

    1.redies 使用缓存 2.redies 缓存结果序列化和反序列化

--- demo/src/main/java/com/example/demo/configuration/RedisConfig.java
+++ demo/src/main/java/com/example/demo/configuration/RedisConfig.java
@@ -1,16 +1,23 @@
 package com.example.demo.configuration;
 
 import com.example.demo.dao.User2;
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.PropertyAccessor;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.CachingConfigurerSupport;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cache.interceptor.KeyGenerator;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.cache.RedisCacheWriter;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
-import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.*;
 
 import java.lang.reflect.Method;
 import java.net.UnknownHostException;
@@ -49,6 +56,30 @@ public class RedisConfig extends CachingConfigurerSupport {
         Jackson2JsonRedisSerializer  jackson2JsonRedisSerializer = new Jackson2JsonRedisSerializer<User2>(User2.class);
         template.setDefaultSerializer(jackson2JsonRedisSerializer);
         return template;
+    }
+
+    //容器会自动检测到这个CacheManager,并替换原来自带的CacheManager
+    @Primary //若配置多个缓存管理器需要有一个默认的缓存管理器
+    @Bean
+    public RedisCacheManager myCacheManager(RedisConnectionFactory redisConnectionFactory){
+        RedisSerializer<String> redisSerializer = new StringRedisSerializer();
+        //.entryTtl(Duration.ofHours(1)); // 设置缓存有效期一小时
+        Jackson2JsonRedisSerializer jackson2JsonRedisSerializer = new Jackson2JsonRedisSerializer(Object.class);
+        ObjectMapper om = new ObjectMapper();
+        om.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
+        om.enableDefaultTyping(ObjectMapper.DefaultTyping.NON_FINAL);
+        jackson2JsonRedisSerializer.setObjectMapper(om);
+
+        // 配置序列化（解决乱码的问题）
+        RedisCacheConfiguration config = RedisCacheConfiguration.defaultCacheConfig()
+                .serializeKeysWith(RedisSerializationContext.SerializationPair.fromSerializer(redisSerializer))
+                .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(jackson2JsonRedisSerializer))
+                .disableCachingNullValues();
+
+        RedisCacheManager cacheManager = RedisCacheManager.builder(redisConnectionFactory)
+                .cacheDefaults(config)
+                .build();
+        return cacheManager;
     }
 
 }
---
+++ demo/src/main/java/com/example/demo/controller/UserController.java
@@ -0,0 +1,20 @@
+package com.example.demo.controller;
+
+import com.example.demo.dao.User2;
+import org.springframework.cache.annotation.CacheConfig;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+@CacheConfig(cacheNames = "emp",cacheManager = "myCacheManager")
+@RestController
+public class UserController {
+    @RequestMapping("/getUser")
+    @Cacheable(value="user-key")
+    public User2 getUser() {
+        User2 user=new User2("aa@126.com", "aa", "aa123456", "aa","123");
+        System.out.println("若下面没出现“无缓存的时候调用”字样且能打印出数据表示测试成功");
+        return user;
+    }
+
+}
+
